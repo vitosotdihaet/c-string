dd34379fac3037aaa078557295b6cfd49f44b4dc
vitosotdihaet <vitostikitos@gmail.com>
2022-11-14 22:09:10 +0300

    Rewrite comments

--- string.c
+++ string.c
@@ -38,26 +38,13 @@ int free_string(string* s) {
     return SUCCESS;
 }
 
-// Adds memory
+// Reallocates memory
 int string_resize(string* s, uint64_t capacity) {
     s->values = reallocate(s->values, char, capacity);
     return SUCCESS;
 }
 
-// Adds a char to string, adding more memory if needed 
-int add_char(string* s, char value) {
-    int result = UNDEFINED;
-    if (s->last_element < s->memory_size) {
-        result = SUCCESS;
-    } else {
-        result = string_resize(s, 2);
-    }
-    s->values[s->last_element] = value;
-    s->last_element++;
-    return result;
-}
-
-// Copies s2 to s1 (taking minimal memory size) -> s1='aboba', s2='bobr' -> s1='bobr '
+// Copies s2 to s1 (taking minimal memory size): s1='aboba', s2='bobr' -> s1='bobr '
 int copy_string(string* s1, string s2) {
     int result = UNDEFINED;
     uint64_t minimal = min(s1->last_element, s2.last_element);
