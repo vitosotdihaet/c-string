392b1b97f62222678a59d93d90a8c6529a93e95e
vitosotdihaet <vitostikitos@gmail.com>
2022-11-17 12:26:51 +0300

    Refactor code, change a few funcs

--- string.c
+++ string.c
@@ -27,21 +27,46 @@ int init_string(string* s) {
     s->memory_size = INIT_CAPACITY;
     s->values = new(char, INIT_CAPACITY);
     s->last_element = 0;
+    s->values[0] = '\0';
     return result;
-}
-
-// Sets all struct's parameters to zero
-int free_string(string* s) {
-    s->values = new(char, 0);
-    s->last_element = 0;
-    s->memory_size = 0;
-    return SUCCESS;
 }
 
 // Reallocates memory
 int string_resize(string* s, uint64_t capacity) {
     s->values = reallocate(s->values, char, capacity);
     return SUCCESS;
+}
+
+// Pushes a char to string 
+int push_char(string* s, char value) {
+    int result = UNDEFINED;
+    if (s->last_element < s->memory_size - 1) {
+        result = SUCCESS;
+    } else {
+        result = string_resize(s, 2);
+    }
+    s->values[s->last_element] = value;
+    s->values[s->last_element + 1] = '\0';
+    s->last_element++;
+    return result;
+}
+
+// Pushes native C string to string
+int push_chars(string* s, char values[]) {
+    int result = UNDEFINED;
+    uint64_t l = strlen(values);
+    for (uint64_t i = 0; i < l; ++i) {
+        result = push_char(s, values[i]);
+        if (result != SUCCESS) return result;
+    }
+    return result;
+}
+
+// Pushes whole string2 to string1
+int push_string(string* s1, string s2) {
+    int result = UNDEFINED;
+    result = push_chars(s1, s2.values);
+    return result;
 }
 
 // Copies s2 to s1 (taking minimal memory size): s1='aboba', s2='bobr' -> s1='bobr '
@@ -59,57 +84,26 @@ int copy_string(string* s1, string s2) {
     return result;
 }
 
-// Pushes a char to string 
-int push_char(string* s, char value) {
+// Fully changes parameters of s1 to s2
+int set_string(string* s1, string s2) {
     int result = UNDEFINED;
-    if (s->last_element < s->memory_size) {
-        result = SUCCESS;
-    } else {
-        result = string_resize(s, 2);
-    }
-    s->values[s->last_element] = value;
-    s->last_element++;
+    result = init_string(s1);
+    result = push_string(s1, s2);
     return result;
 }
 
-// Pushes whole string2 to string1
-int push_string(string* s1, string s2) {
-    int result = UNDEFINED;
-    for (uint64_t i = 0; i < s2.last_element; ++i) {
-        result = push_char(s1, s2.values[i]);
-        if (result != SUCCESS) { return result; }
-    }
-    return SUCCESS;
-}
-
-// Pushes native C string to string
-int push_chars(string* s, char values[]) {
-    int result = UNDEFINED;
-    uint64_t l = strlen(values);
-    for (uint64_t i = 0; i < l; ++i) {
-        result = push_char(s, values[i]);
-        if (result != SUCCESS) { return result; }
-    }
-    return SUCCESS;
-}
-
-// Fully changes parameters of s1 to s2
-int set_string(string* s1, string s2) {
-    *s1 = s2;
-    return SUCCESS;
-}
-
-// Reads string char by char returning -1 if EOF
+// Reads string char by char returning 1 or -1 if EOF
 int read_string(string* s) {
+    init_string(s);
     char c = ' ';
-    int end = 0;
+    int end = 1;
 
     while (1) {
         c = getchar();
         if (c == ' ' || c == ',' || c == '\t' || c == '\n') {
             break;
         } else if ((int) c == EOF) {
-            end = -1;
+            end = EOF;
             break;
         }
         push_char(s, c);
--- string.h
+++ string.h
@@ -13,14 +13,8 @@ typedef struct string {
 // Initialize string with default capacity
 int init_string(string* s);
 
-// Sets all struct's parameters to zero
-int free_string(string* s);
-
 // Adds memory
 int string_resize(string* s, uint64_t capacity);
-
-// Copies s2 to s1 (taking minimal memory size) -> s1='aboba', s2='bobr' -> s1='bobr '
-int copy_string(string* s1, string s2);
 
 // Adds a char to string, adding more memory if needed 
 int push_char(string* s, char value);
@@ -30,6 +24,9 @@ int push_string(string* s1, string s2);
 
 // Pushes native C string, adding more memory if needed
 int push_chars(string* s, char values[]);
+
+// Copies s2 to s1 (taking minimal memory size) -> s1='aboba', s2='bobr' -> s1='bobr '
+int copy_string(string* s1, string s2);
 
 // Fully changes parameters of s1 to s2
 int set_string(string* s1, string s2);
