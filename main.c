29d82c8da3d6c818f3865d7ca9f5654064b9760a
vitosotdihaet <vitostikitos@gmail.com>
2024-02-23 19:56:17 +0300

    Make it an STB-style library

--- main.c
+++ main.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
+#include <inttypes.h>
+
+#define _STRING_IMPLEMENTATION
 #include "string.h"
+
 
 int main() {
     string s1;
@@ -24,7 +28,7 @@ int main() {
 
     printf("Input any string to s1: ");
     string_read(&s1);
-    printf("s1 after reading: %s. It's length is %lld\n", s1.values, string_len(s1));
+    printf("s1 after reading: %s. It's length is %"PRIu32"\n", s1.values, string_len(s1));
 
     string_set(&s2, s1);
     printf("s2 after setting it to s1: %s\n", s2.values);
@@ -36,7 +40,7 @@ int main() {
     printf("s2 after pushing s1 to it: %s\n", s2.values);
 
     if (string_contains(s2, 'a')) {
-        printf("s2 contains 'a'! It's index is %d!\n", string_index(s2, 'a'));
+        printf("s2 contains 'a'! It's index is %"PRIi64"!\n", string_index(s2, 'a'));
     } else {
         printf("s2 doesn't contain 'a' :(\n");
     }
--- makefile
+++ makefile
@@ -2,8 +2,5 @@ CC = gcc
 
 CFLAGS = -Wall -Wextra
 
-a: string.o
-	$(CC) $(CFLAGS) -o main.out main.c string.o
-
-string:
-	$(CC) $(CFLAGS) -c string.c
+main: string.h
+	$(CC) $(CFLAGS) -o main.out main.c
